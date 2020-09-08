be574df02e80eff95f95de01f645d92a222b0f72
fthomp8044 <fthomp2430@gmail.com>
2020-09-08 13:58:35 -0400

    update header

--- keeper-pt3/keeperpt3/src/components/App.js
+++ keeper-pt3/keeperpt3/src/components/App.js
@@ -31,7 +31,6 @@ function App() {
       {notes.map((noteItem, index) => {
         return <Note key={index} id={index} title={noteItem.title} content={noteItem.content} onDelete={deleteNote}/>
       })}
-      // <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
