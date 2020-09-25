cba02c3dd20d6a6f73d7243d23ab5d76e1e3151f
fthomp8044 <fthomp2430@gmail.com>
2020-09-25 00:00:28 -0400

    gave poject some polish and style

--- django_portfolio-project/personal_portfolio/blog/models.py
+++ django_portfolio-project/personal_portfolio/blog/models.py
@@ -5,3 +5,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateTimeField()
     blog_description = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.title
--- django_portfolio-project/personal_portfolio/blog/urls.py
+++ django_portfolio-project/personal_portfolio/blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 from . import views
 # ^^^ means all views in the current directory of blog
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
+    path('<int:blog_id>/', views.detail, name='detail'),
 ]
